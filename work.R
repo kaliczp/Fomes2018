d0c3365af9bee62883f76a701ee0eb4a91ce6722
Péter Kalicz <kaliczp@gmail.com>
2018-01-10 14:25:18 +0100

    mont reorder

--- work.R
+++ work.R
@@ -7,3 +7,4 @@ colnames(Ah.ok) <- month.abb[c(10:12,1:8)]
 rownames(Ah.ok) <- Ah.spec[1:10]
 heatmap(as.matrix(Ah.ok))
 heatmap(as.matrix(Ah.ok), margins = c(4,15), Colv=NA, Rowv=NA)
+heatmap(as.matrix(Ah.ok[,c(4:11,1:3)]), margins = c(4,15), Colv=NA, Rowv=NA)
