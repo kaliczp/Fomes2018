58748075b2bb9f7cc60a3ce0a7771ad509653381
Péter Kalicz <kaliczp@gmail.com>
2018-01-10 14:12:17 +0100

    heatmap

--- work.R
+++ work.R
@@ -1,5 +1,8 @@
 ## Read in
 Ah <- read.csv2("Adatok.csv", fileEncoding = "latin1")
 Ah <- read.csv2("Adatok.csv")
-Ah.ok <- Ah[-c(11,12),-c(13,14)]
+Ah.spec <- Ah[,1]
+Ah.ok <- Ah[-c(11,12),-c(1,13,14)]
 colnames(Ah.ok) <- month.abb[c(10:12,1:8)]
+rownames(Ah.ok) <- Ah.spec[1:10]
+heatmap(as.matrix(Ah.ok))
