436f1b37732555db9629f4c5c4847dba8afa131f
Péter Kalicz <kaliczp@gmail.com>
2018-01-10 14:37:43 +0100

    no scaling

--- work.R
+++ work.R
@@ -7,4 +7,6 @@ colnames(Ah.ok) <- month.abb[c(10:12,1:8)]
 rownames(Ah.ok) <- Ah.spec[1:10]
 heatmap(as.matrix(Ah.ok))
 heatmap(as.matrix(Ah.ok), margins = c(4,15), Colv=NA, Rowv=NA)
-heatmap(as.matrix(Ah.ok[,c(4:11,1:3)]), margins = c(4,15), Colv=NA, Rowv=NA)
+heatmap(as.matrix(Ah.ok[,c(4:11,1:3)]), margins = c(4,15), Colv=NA, Rowv=NA, col=topo.colors(50))
+heatmap(as.matrix(Ah.ok[,c(4:11,1:3)]), margins = c(4,15), Colv=NA, Rowv=NA, col=terrain.colors(10))
+heatmap(as.matrix(Ah.ok[,c(4:11,1:3)]), margins = c(4,15), Colv=NA, Rowv=NA, scale="none")
